38101af22eea41cedb2e4367b98bd45a04bd097f
DeepikaGera <60508103+DeepikaGera@users.noreply.github.com>
2020-02-07 11:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world11111111'}
+        return {'hello': 'world from ultrahook'}
 
 api.add_resource(HelloWorld, '/')
 
